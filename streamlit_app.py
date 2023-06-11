a959f041a2d2708328f9b561117db3ee864bf9c9
lse02 <22sweeen@gmail.com>
2023-06-12 00:03:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from streamlit.components.v1 import html
 
 # Read the HTML file
-with open('/Users/user/Desktop/learnings/google/cafe_map.html', 'r') as file:
+with open('cafe_map.html', 'r') as file:
     html_content = file.read()
 
 # Display the HTML content in Streamlit
