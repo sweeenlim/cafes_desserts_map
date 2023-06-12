78be9545aadb91bc05c23d9937385706e7ebbeed
--unset <e0772420@u.nus.edu>
2023-06-12 17:36:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 
 from streamlit.components.v1 import html
 
+st.header("Cafes and Desserts in Singapore Map")
+st.subheader("A map to show the cafes in Singapore and desserts within 500m proximity of the cafes")
+
 # Read the HTML file
 with open('cafe_map.html', 'r') as file:
     html_content = file.read()
